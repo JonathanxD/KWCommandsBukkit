91839fd949fbadd43f165805303cb5bc17fb22b4
Jonathan <joniweb01@gmail.com>
2021-04-03 02:12:35 -0300

    1.1.16: License & Gradle

--- src/main/java/com/github/jonathanxd/kwcommandsbukkit/common/CommonArguments.java
+++ src/main/java/com/github/jonathanxd/kwcommandsbukkit/common/CommonArguments.java
@@ -3,7 +3,7 @@
  *
  *         The MIT License (MIT)
  *
- *      Copyright (c) 2018 JonathanxD <jonathan.scripter@programmer.net>
+ *      Copyright (c) 2021 JonathanxD <jonathan.scripter@programmer.net>
  *      Copyright (c) contributors
  *
  *
